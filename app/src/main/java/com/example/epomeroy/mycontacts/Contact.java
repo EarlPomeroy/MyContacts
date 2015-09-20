38f3b27d5d6410c1168bec450d92b6474a6ed0f7
Earl Pomeroy <earl.pomeroy42@gmail.com>
2015-09-20 17:58:53 -0600

    Added contact list. Style and layed out list.

---
+++ app/src/main/java/com/example/epomeroy/mycontacts/Contact.java
@@ -0,0 +1,16 @@
+package com.example.epomeroy.mycontacts;
+
+/**
+ * Created by epomeroy on 9/20/15.
+ */
+public class Contact {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/epomeroy/mycontacts/ContactListActivity.java
+++ app/src/main/java/com/example/epomeroy/mycontacts/ContactListActivity.java
@@ -1,16 +1,74 @@
 package com.example.epomeroy.mycontacts;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ContactListActivity extends AppCompatActivity {
+
+    private class ContactAdapter extends ArrayAdapter<Contact> {
+        public ContactAdapter(ArrayList<Contact> contacts) {
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);
+
+            TextView nameTextView = (TextView) convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+
+            return convertView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            Contact c = new Contact();
+            c.setName("Fuck U. Bob");
+            contacts.add(c);
+        }
+
+        ListView contactListView = (ListView) findViewById(R.id.contact_list_view);
+        contactListView.setAdapter(new ContactAdapter(contacts));
+        contactListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            private int previousFirstItem = 0;
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem > previousFirstItem) {
+                    getSupportActionBar().hide();
+                }
+                else if (firstVisibleItem < previousFirstItem) {
+                    getSupportActionBar().show();
+                }
+
+                previousFirstItem = firstVisibleItem;
+            }
+        });
     }
 
     @Override
