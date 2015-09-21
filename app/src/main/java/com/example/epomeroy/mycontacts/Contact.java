f68fecb3baef3141f48ab14b18d4bf6be1b7bb57
Earl Pomeroy <earl.pomeroy42@gmail.com>
2015-09-21 00:36:14 -0600

    Updated contact view with tool bar and icons. Started adding field list of phone numbers and emails.

--- app/src/main/java/com/example/epomeroy/mycontacts/Contact.java
+++ app/src/main/java/com/example/epomeroy/mycontacts/Contact.java
@@ -1,12 +1,47 @@
 package com.example.epomeroy.mycontacts;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by epomeroy on 9/20/15.
  */
 public class Contact implements Serializable {
     private String name;
+    private ArrayList<String> emails = new ArrayList<>();
+    private ArrayList<String> phoneNumbers = new ArrayList<>();
+
+    public ArrayList<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public ArrayList<String> getEmails() {
+        return emails;
+    }
+
+    public void addEmail(String email) {
+        this.emails.add(email);
+    }
+
+    public void removeEmail(String email) {
+        this.emails.remove(email);
+    }
+
+    public void removeEmail(int emailIndex) {
+        this.emails.remove(emailIndex);
+    }
+
+    public void addPhoneNumber(String phoneNumber) {
+        this.phoneNumbers.add(phoneNumber);
+    }
+
+    public void removePhoneNumber(String phoneNumber) {
+        this.phoneNumbers.remove(phoneNumber);
+    }
+
+    public void removePhoneNumber(int phoneNumberIndex) {
+        this.phoneNumbers.remove(phoneNumberIndex);
+    }
 
     public String getName() {
         return name;
--- app/src/main/java/com/example/epomeroy/mycontacts/ContactViewActivity.java
+++ app/src/main/java/com/example/epomeroy/mycontacts/ContactViewActivity.java
@@ -1,18 +1,55 @@
 package com.example.epomeroy.mycontacts;
 
 import android.graphics.Point;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ContactViewActivity extends AppCompatActivity {
+    private static final String TAG = "ContactViewActivity";
     public static final String EXTRA = "CVA_Contact";
+
+    private class FieldsAdapter extends BaseAdapter {
+        private ArrayList<String> emails;
+        private ArrayList<String> phoneNumbers;
+
+        public FieldsAdapter(ArrayList<String> emails, ArrayList<String> phoneNumbers) {
+            emails = emails;
+            phoneNumbers = phoneNumbers;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +70,23 @@ public class ContactViewActivity extends AppCompatActivity {
         Contact contact = (Contact) getIntent().getSerializableExtra(EXTRA);
         TextView contactName = (TextView) findViewById(R.id.contact_view_name);
         contactName.setText(contact.getName());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.contact_view_toolbar);
+        this.setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getGroupId();
+                if (id == R.id.contact_view_edit) {
+                    Log.d(TAG, "Clicked edit icon");
+                    return true;
+                }
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu_contact_view);
+
+        ListView listView = (ListView) findViewById(R.id.contact_view_fields);
     }
 
     @Override
